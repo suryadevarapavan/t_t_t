d60e46995886dcac390dd0d483472f1f42b6b17b
P1ttadu <suryadevarapavan770@gmail.com>
2024-07-21 22:11:13 +0530

    Add files via upload

---
+++ tiktaktoe.py
@@ -0,0 +1,32 @@
+A=[[" "," "," "],[" "," "," "],[" "," "," "]]
+i=0
+def disp():
+    print(A[0][0],"|",A[0][1],"|",A[0][2])
+    print("----------")
+    print(A[1][0],"|",A[1][1],"|",A[1][2])
+    print("----------")
+    print(A[2][0],"|",A[2][1],"|",A[2][2])
+
+while(1):
+    if(i>3):
+        print("Game OVER")
+        break;
+    print("PLAYER_ONE")
+    m=int(input("Enter value for m:"))
+    n=int(input("Enter value for n:"))
+    A[m-1][n-1]="X"
+    disp()
+    if( "X"==A[0][0]==A[1][1]==A[2][2] or "X"==A[0][0]==A[0][1]==A[0][2] or "X"==A[1][0]==A[1][1]==A[1][2] or "X"==A[2][0]==A[2][1]==A[2][2] or "X"==A[0][0]==A[1][0]==A[2][0] or "X"==A[0][1]==A[1][1]==A[2][1] or "X"==A[0][2]==A[1][2]==A[2][2]):
+        print("Player 0NE Wins")
+        break;
+    print("PLAYER_TWO")
+    m=int(input("Enter value for m:"))
+    n=int(input("Enter value for n:"))
+    A[m-1][n-1]="O"
+    disp()
+    if( "X"==A[0][0]==A[1][1]==A[2][2] or "X"==A[0][0]==A[0][1]==A[0][2] or "X"==A[1][0]==A[1][1]==A[1][2] or "O"==A[2][0]==A[2][1]==A[2][2] or "O"==A[0][0]==A[1][0]==A[2][0] or "O"==A[0][1]==A[1][1]==A[2][1] or "O"==A[0][2]==A[1][2]==A[2][2]):
+        print("Player TW0 Wins")
+        break;
+    i=i+1
+
+                 
